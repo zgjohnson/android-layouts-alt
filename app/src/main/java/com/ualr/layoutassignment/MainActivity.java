afbff9d7c9d54ea78ad57ea93d8229d4d5a5a6ff
irconde <irconde@ualr.edu>
2020-08-06 18:47:22 -0500

    Fist code commit

---
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ualr.layoutassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
+
+    // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
